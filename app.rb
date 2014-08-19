ef42851db0d457e46bb7a5adf43259a1a66dcb35
Ashley <a.clarkem@gmail.com>
2014-08-19 10:37:53 -0600

    hash but in incorrect format

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'csv'
+require 'pp'
 
-CSV.foreach('data/gschool_commute_data.csv') do |row|
-  p row.inspect
+CSV.foreach('data/gschool_commute_data.csv', headers: true) do |row|
+  hashes = row.to_h
+  pp hashes
 end
+
+
+
+
+
+
+
+
+
+
