16108901b07c810f2dfd9b4f959253344b859053
Ashley <a.clarkem@gmail.com>
2014-08-20 09:55:34 -0600

    hash set up properly

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
 require 'csv'
 require 'pp'
 
-CSV.foreach('data/gschool_commute_data.csv', headers: true) do |row|
-  hashes = row.to_h
-  pp hashes
+
+csv = CSV.read('data/gschool_commute_data.csv', headers: true)
+commutes = []
+csv.each do |row|
+  commutes.push(row.to_h)
 end
 
+by_person = commutes.group_by{|hash| hash['Person']}
 
+by_person.each_value do |values|
+  values.each do |hashes|
+    hashes.each do |key|
+      if key == "Person"
+        hashes.delete(key)
+      end
+    end
+  end
+end
 
-
+pp by_person
 
 
 
