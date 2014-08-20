3967d2e91ab4027bae238ac69d0fa697092493a8
Ashley <a.clarkem@gmail.com>
2014-08-20 16:09:19 -0600

    finished

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'csv'
 require 'pp'
 
-
+# Set up hash:
 csv = CSV.read('data/gschool_commute_data.csv', headers: true)
 commutes = []
 csv.each do |row|
@@ -20,6 +20,8 @@ by_person.each_value do |values|
   end
 end
 
+
+# Nate's commute time:
 by_person["Nate"].each do |hash|
   if hash["Week"] == "4" && hash["Day"] == "Wednesday"
     pp "Nate's time: #{hash["Inbound"]}"
@@ -27,6 +29,7 @@ by_person["Nate"].each do |hash|
 end
 
 
+# Average commute times:
 x = (by_person.values).flatten
   inbound = x.map{|time|time["Inbound"]}
   outbound = x.map{|time|time["Outbound"]}
@@ -46,6 +49,29 @@ puts "Average time outbound: #{outbound_sum/outbound.length}"
 puts "Average total: #{(inbound_sum + outbound_sum)/(inbound.length + outbound.length)}"
 
 
+#Fastest walk time:
+by_person.each do |person, array|
+  array.delete_if do |hash|
+    hash["Mode"] != "Walk"
+  end
+end
+
+by_person.delete_if{|key, value| value == []}
+
+by_person.each do |person, array|
+  array.map!{|time|time["Inbound"].to_i + time["Outbound"].to_i}
+end
+
+emily = by_person["Emily"]
+emily_sum = 0
+emily.each{|time|emily_sum += time}
 
 
+rachel = by_person["Rachel"]
+rachel_sum = 0
+rachel.each{|time|rachel_sum += time}
 
+puts "Is Rachel faster than Emily?"
+pp emily_sum > rachel_sum
+
+puts "Emily's average time is: #{emily_sum/emily.count}"
